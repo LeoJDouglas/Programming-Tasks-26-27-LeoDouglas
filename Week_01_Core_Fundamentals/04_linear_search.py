7b2e5e0adba2048ce5f8d81dce2403832d889e3e
LeoJDouglas <45852@rws.uk.net>
2026-09-10 08:57:39 +0100

    hello world

--- Week_01_Core_Fundamentals/04_linear_search.py
+++ Week_01_Core_Fundamentals/04_linear_search.py
@@ -18,7 +18,15 @@ def main():
     # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
     # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
     pass
-
-
+import string
+import random
 if __name__ == "__main__":
+    num = []
+    list = ''.join(random.choices(string.ascii_lowercase, k=10))
+    target = input("Enter the value to search for: ")
+    for i in range(len(list)):
+        if list[i] == target:
+            num.append(i)
+    print("The target value is found at index: " + str(num))
+
     main()
